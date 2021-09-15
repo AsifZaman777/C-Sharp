4a5c5b68097f0a25d85b0ab896db5e3627304e41
Asif Zaman <69311703+AsifZaman777@users.noreply.github.com>
2021-09-16 03:23:47 +0600

    Void Function

---
+++ C# Fuctions/1. Function/1. Function/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.Function
+{
+    class Program
+    {
+        static void  def(string s)
+        {
+            Console.WriteLine(s);
+        }
+
+        static void def_parameter(string s="default")
+        {
+            Console.WriteLine(s);
+        }
+
+        // key:value pair argument
+
+        static void keyVal_arg(string arg1, string arg2, string arg3)
+        {
+            Console.WriteLine(arg1+" and "+arg2+" are friennds "); //Angela rayed are friends
+        }
+
+        static void KeyValArg(string arg1="Asif",string arg2="Prottoy",string arg3="Ovy")
+        {
+            Console.WriteLine(arg1 + " kikced " + arg2 + " Ass ");
+        }
+
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            def(s); // return string 
+
+            def_parameter("Asif"); //return Asif
+            def_parameter("Prottoy"); // return Prottoy
+            def_parameter(); // return default
+
+            keyVal_arg(arg1: "Angela", arg2: "Rayed", arg3: "Alif");
+            KeyValArg();
+
+        }
+    }
+}
